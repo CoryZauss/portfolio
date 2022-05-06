214f9e34e194ec7a0c7c42e24d4dcb77d1e7044a
Cory Zauss <coryclassic1989@gmail.com>
2022-05-06 14:23:15 -0400

    update to fix next using document

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,50 +1,26 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import moon from "../assets/icons/moon-solid.svg";
 import sun from "../assets/icons/sun-solid.svg";
-import { useEffect, useState } from "react";
 
-export default function Nav() {
-  const [darkTheme, setDarkTheme] = useState("");
+export default function Nav({darktheme, changetheme}) {
 
-  const handleToggle = (e) => {
-    setDarkTheme(e.target.checked);
-  };
-
-  useEffect(() => {
-    if (darkTheme !== "") {
-      if (darkTheme) {
-        document.documentElement.setAttribute("data-theme", "dark");
-        window.localStorage.setItem("theme", "dark");
-      } else {
-        document.documentElement.removeAttribute("data-theme");
-        window.localStorage.setItem("theme", "light");
-      }
-    }
-  }, [darkTheme]);
-
-  useEffect(() => {
-    const root = window.document.documentElement;
-    const initialColorValue = root.style.getPropertyValue(
-      "--initial-color-mode"
-    );
-    setDarkTheme(initialColorValue === "dark");
-  }, []);
   return (
     <div className="nav-container">
-      <div className="sun-moon">
-        {darkTheme === true ? (
+      {/* <div className="sun-moon">
+        {darktheme === true ? (
           <Image src={moon} alt="moon" className="theme-moon" />
         ) : (
           <Image src={sun} alt="sun" className="theme-sun" />
         )}
       </div>
-      <form action="#">
+      {darktheme !== '' && <form action="#">
         <label className="switch">
-          <input type="checkbox" checked={darkTheme} onChange={handleToggle} />
+          <input type="checkbox" checked={darktheme} onChange={changetheme} />
           <span className="slider"></span>
         </label>
-      </form>
+      </form>} */}
 
       <Link href="/">
         <a className="navlink">Home</a>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
+// import Head from "next/head";
+// import Image from "next/image";
 import Nav from "../components/Nav";
 
 export default function Projects() {
