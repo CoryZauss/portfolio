b599c4dad1873d88b4b005d38eb8d7c189ca07ea
Cory Zauss <coryclassic1989@gmail.com>
2022-05-06 14:26:34 -0400

    move dark logic to nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,55 @@
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import moon from "../assets/icons/moon-solid.svg";
 import sun from "../assets/icons/sun-solid.svg";
+import { useEffect, useState } from "react";
 
 export default function Nav({darktheme, changetheme}) {
+  const [darkTheme, setDarkTheme] = useState('');
+
+  const handleToggle = (event) => {
+    setDarkTheme(event.target.checked);
+  };
+
+  useEffect(() => {
+    if (darkTheme !== '') {
+      if (darkTheme) {
+        // Set value of  darkmode to dark
+        document.documentElement.setAttribute("data-theme", "dark");
+        window.localStorage.setItem("theme", "dark");
+      } else {
+        // Set value of  darkmode to light
+        document.documentElement.removeAttribute("data-theme");
+        window.localStorage.setItem("theme", "light");
+      }
+    }
+  }, [darkTheme]);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    console.log(root.style)
+    const initialColorValue = root.style.getPropertyValue(
+      "--initial-color-mode"
+    );
+    // Set initial darkmode to light
+    setDarkTheme(initialColorValue === "dark");
+  }, []);
 
   return (
     <div className="nav-container">
-      {/* <div className="sun-moon">
-        {darktheme === true ? (
+      <div className="sun-moon">
+        {darkTheme === true ? (
           <Image src={moon} alt="moon" className="theme-moon" />
         ) : (
           <Image src={sun} alt="sun" className="theme-sun" />
         )}
       </div>
-      {darktheme !== '' && <form action="#">
+      {darkTheme !== '' && <form action="#">
         <label className="switch">
-          <input type="checkbox" checked={darktheme} onChange={changetheme} />
+          <input type="checkbox" checked={darkTheme} onChange={handleToggle} />
           <span className="slider"></span>
         </label>
-      </form>} */}
+      </form>}
 
       <Link href="/">
         <a className="navlink">Home</a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,63 +8,13 @@ import moon from "../assets/icons/moon-solid.svg";
 import sun from "../assets/icons/sun-solid.svg";
 
 const Home = () => {
-  const [darkTheme, setDarkTheme] = useState(undefined);
 
-  const handleToggle = (event) => {
-    setDarkTheme(event.target.checked);
-  };
-
-  useEffect(() => {
-    if (darkTheme !== undefined) {
-      if (darkTheme) {
-        // Set value of  darkmode to dark
-        document.documentElement.setAttribute("data-theme", "dark");
-        window.localStorage.setItem("theme", "dark");
-      } else {
-        // Set value of  darkmode to light
-        document.documentElement.removeAttribute("data-theme");
-        window.localStorage.setItem("theme", "light");
-      }
-    }
-  }, [darkTheme]);
-
-  useEffect(() => {
-    const root = window.document.documentElement;
-    console.log(root.style)
-    const initialColorValue = root.style.getPropertyValue(
-      "--initial-color-mode"
-    );
-    // Set initial darkmode to light
-    setDarkTheme(initialColorValue === "dark");
-  }, []);
 
   return (
     <>
       <div className="page-container">
-        <div>
-          <div className="sun-moon">
-            {darkTheme === true ? (
-              <Image src={moon} alt="moon" className="theme-moon" />
-            ) : (
-              <Image src={sun} alt="sun" className="theme-sun" />
-            )}
-          </div>
-          <div>
-            {darkTheme !== undefined && (
-              <form action="#">
-                <label className="switch">
-                  <input
-                    type="checkbox"
-                    checked={darkTheme}
-                    onChange={handleToggle}
-                  />
-                  <span className="slider"></span>
-                </label>
-              </form>
-            )}
-          </div>
-        </div>
-        <Nav darktheme={darkTheme} changetheme={handleToggle} />
+
+        <Nav  />
         <main className="main">
           <div className="my-pic">
             <Image alt="me" src={me} />
